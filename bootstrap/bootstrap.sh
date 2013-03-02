b0e7f645cfc493712de1993adae8a517c9254377
Tom Bombadil <amanibhavam@destructuring.org>
2013-03-01 22:34:23 -0800

    build ami behaves similarly to build vagrant by running new instance with newly built ami and ssh into it

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -28,6 +28,16 @@ function main {
   aptitude clean
   atrm $(at -l | awk '{print $1}')
 
+  # TODO duplication for ec2, see finished.sh
+  # remove cached network configurations
+  rm -fv /etc/udev/rules.d/70-persistent-net.rules
+  mkdir -pv /etc/udev/rules.d/70-persistent-net.rules
+  rm -fv /lib/udev/rules.d/75-persistent-net-generator.rules
+  rm -rfv /dev/.udev/ /var/lib/dhcp3/*
+
+  # don't see a battery
+  rm -fv /etc/dbus-1/system.d/org.freedesktop.UPower.conf
+
   poweroff
 }
 
