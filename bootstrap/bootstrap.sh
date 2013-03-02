74cbdf9d972c3d34369c0c2d188d179e4f9c2470
Tom Bombadil <amanibhavam@destructuring.org>
2013-03-01 22:43:46 -0800

    some of the cleanup items became directories

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -30,7 +30,7 @@ function main {
 
   # TODO duplication for ec2, see finished.sh
   # remove cached network configurations
-  rm -fv /etc/udev/rules.d/70-persistent-net.rules
+  rm -rfv /etc/udev/rules.d/70-persistent-net.rules
   mkdir -pv /etc/udev/rules.d/70-persistent-net.rules
   rm -fv /lib/udev/rules.d/75-persistent-net-generator.rules
   rm -rfv /dev/.udev/ /var/lib/dhcp3/*
--- bootstrap/finished.sh
+++ bootstrap/finished.sh
@@ -2,7 +2,7 @@
 
 function main {
   # remove cached network configurations
-  rm -fv /etc/udev/rules.d/70-persistent-net.rules
+  rm -rfv /etc/udev/rules.d/70-persistent-net.rules
   mkdir -pv /etc/udev/rules.d/70-persistent-net.rules
   rm -fv /lib/udev/rules.d/75-persistent-net-generator.rules
   rm -rfv /dev/.udev/ /var/lib/dhcp3/*
