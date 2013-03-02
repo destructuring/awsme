e214e5e5b981842d39c547f4a01cfd304306676a
Tom Bombadil <amanibhavam@destructuring.org>
2013-03-01 20:16:26 -0800

    move cleaning functions to finished.sh

--- bootstrap/finished.sh
+++ bootstrap/finished.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -exfu
 
 function main {
-  true
+  # remove cached network configurations
+  rm -fv /etc/udev/rules.d/70-persistent-net.rules
+  mkdir -pv /etc/udev/rules.d/70-persistent-net.rules
+  rm -fv /lib/udev/rules.d/75-persistent-net-generator.rules
+  rm -rfv /dev/.udev/ /var/lib/dhcp3/*
+
+  # don't see a battery
+  rm -fv /etc/dbus-1/system.d/org.freedesktop.UPower.conf
 }
 
 main "$@"
--- bootstrap/guest_additions.sh
+++ bootstrap/guest_additions.sh
@@ -16,17 +16,6 @@ function main {
   sh /mnt/VBoxLinuxAdditions.run
   umount /mnt
   rm -f ~/VBoxGuestAdditions.iso
-
-  # remove cached network configurations
-  rm -fv /etc/udev/rules.d/70-persistent-net.rules
-  mkdir -pv /etc/udev/rules.d/70-persistent-net.rules
-  rm -fv /lib/udev/rules.d/75-persistent-net-generator.rules
-  rm -rfv /dev/.udev/ /var/lib/dhcp3/*
-
-  # don't see a battery
-  rm -fv /etc/dbus-1/system.d/org.freedesktop.UPower.conf
-
-  poweroff
 }
 
 main "$@"
