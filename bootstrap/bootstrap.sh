1551d0e5a1f7161a1866c056193a577aad07e1fd
Tom Bombadil <amanibhavam@destructuring.org>
2013-03-05 19:39:39 -0800

    disable more services, same ec2/vagrant packages

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -19,13 +19,27 @@ function main {
 
   # basic packages
   aptitude -y install wget curl netcat git rsync make
+  aptitude -y install dkms
 
   # unecessary daemons
   aptitude -y purge whoopsie acpid nfs-common rpcbind
 
+  # remove crap from vagrant cloud images
+  aptitude -y purge juju puppet graphviz fontconfig libpango1.0-0 libgvc5 \
+              x11-common libxt6 libxmu6 libxaw7 libsm6 libice6 bzr debconf-utils fontconfig \
+              fontconfig-config fonts-liberation libcairo2 libcap2 libcdt4 libcgraph5 \
+              libdatrie1 libfontconfig1 libgd2-noxpm libgraph4 libgssglue1 libgvc5 libgvpr1 \
+              libjpeg8 libjpeg-turbo8 libnfsidmap2 libpango1.0-0 libpathplan4 libpixman-1-0 \
+              libthai0 libthai-data libtirpc1 libxcb-render0 libxcb-shm0 libxft2 libxpm4 \
+              libxrender1 python-bzrlib ttf-dejavu-core ttf-liberation puppet-common \
+              libaugeas0 libfontenc1 libllvm3.0 libxcomposite1 libxdamage1 libxfixes3 \
+              libxfont1 libxkbfile1 libxrandr2 libzookeeper-mt2 puppet-common \
+              python-support python-twisted python-twisted-conch xfonts-base xfonts-utils \
+              xserver-xorg-core
+
   # don't start getty
-  for a in $(set +f; ls -d /etc/init/tty*.conf); do
-    echo "manual" >> "$a"
+  for a in $(set +f; cd /etc/init && ls -d tty*.conf dbus.conf plymouth* setvtrgb.conf upstart-socket-bridge.conf); do
+    echo "manual" >> "/etc/init/$a"
   done
 
   # ruby
@@ -44,9 +58,6 @@ function main {
   mkdir -pv /etc/udev/rules.d/70-persistent-net.rules
   rm -fv /lib/udev/rules.d/75-persistent-net-generator.rules
   rm -rfv /dev/.udev/ /var/lib/dhcp3/*
-
-  # don't see a battery
-  rm -fv /etc/dbus-1/system.d/org.freedesktop.UPower.conf
   ### END finished.sh
 
   poweroff
--- bootstrap/finished.sh
+++ bootstrap/finished.sh
@@ -8,9 +8,6 @@ function main {
   mkdir -pv /etc/udev/rules.d/70-persistent-net.rules
   rm -fv /lib/udev/rules.d/75-persistent-net-generator.rules
   rm -rfv /dev/.udev/ /var/lib/dhcp3/*
-
-  # don't see a battery
-  rm -fv /etc/dbus-1/system.d/org.freedesktop.UPower.conf
 }
 
 main "$@"
