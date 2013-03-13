292c1d956a41c8a24522fd579e3a4aa3cf0fb222
Tom Bombadil <amanibhavam@destructuring.org>
2013-03-12 19:43:04 -0700

    respect AWSME in recipes

--- cookbooks/awsme/recipes/project.rb
+++ cookbooks/awsme/recipes/project.rb
@@ -1,10 +1,10 @@
 require 'yaml'
-aws_secrets = YAML.load(File.read(File.join(node[:release_dir], "config", "aws.yml")))
+aws_secrets = YAML.load(File.read("#{File.join(ENV['AWSME']}/config/aws.yml"))
 
 node.default[:aws][:access_key] = aws_secrets["AWS_ACCESS_KEY"]
 node.default[:aws][:secret_key] = aws_secrets["AWS_SECRET_KEY"]
 
-template "#{node[:release_dir]}/config/aws.txt" do
+template "#{ENV['AWSME']}/config/aws.txt" do
   source "aws.txt.erb"
   mode 0600
 end
