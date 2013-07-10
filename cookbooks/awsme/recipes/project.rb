7e3bb49b019643d4fbb2aeda61ce1e4051e1380c
Tom Bombadil <amanibhavam@destructuring.org>
2013-07-10 16:07:52 -0700

    also read :aws_access_key_id and :aws_secret_access_key

--- cookbooks/awsme/recipes/project.rb
+++ cookbooks/awsme/recipes/project.rb
@@ -1,8 +1,8 @@
 require 'yaml'
 aws_secrets = YAML.load(File.read("#{ENV['AWSME']}/config/aws.yml"))
 
-node.default[:aws][:access_key] = aws_secrets["AWS_ACCESS_KEY"]
-node.default[:aws][:secret_key] = aws_secrets["AWS_SECRET_KEY"]
+node.default[:aws][:access_key] = aws_secrets[:aws_access_key_id] || aws_secrets["AWS_ACCESS_KEY"]
+node.default[:aws][:secret_key] = aws_secrets[:aws_secret_access_key] || aws_secrets["AWS_SECRET_KEY"]
 
 template "#{ENV['AWSME']}/config/aws.txt" do
   source "aws.txt.erb"
