a895a9cfe28cd2daef90803e8fc26d4b5db73da9
David Nghiem <nghidav@gmail.com>
2014-06-04 13:47:29 -0700

    dont write out aws.txt

--- cookbooks/awsme/recipes/project.rb
+++ cookbooks/awsme/recipes/project.rb
@@ -4,7 +4,7 @@ aws_secrets = YAML.load(File.read("#{ENV['AWSME']}/config/aws.yml"))
 node.default[:aws][:access_key] = aws_secrets["aws_access_key_id"] || aws_secrets["AWS_ACCESS_KEY"]
 node.default[:aws][:secret_key] = aws_secrets["aws_secret_access_key"] || aws_secrets["AWS_SECRET_KEY"]
 
-template "#{ENV['AWSME']}/config/aws.txt" do
-  source "aws.txt.erb"
-  mode 0600
-end
+#template "#{ENV['AWSME']}/config/aws.txt" do
+#  source "aws.txt.erb"
+#  mode 0600
+#end
